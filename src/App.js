3e67998f81f62558ca53e2e094efbd95ceb5bbce
Mariya Moiseeva <mash.dobarina@gmail.com>
2023-07-17 16:08:42 +0300

    Установка и настройка moment

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
+import moment from "moment";
 import Body from "./components/Body";
 import Header from "./components/Header";
 
 function App() {
+  window.moment = moment;
+  moment.updateLocale('en', {week: {dow: 1}});
+  
   return (
     <>
       <Header />
