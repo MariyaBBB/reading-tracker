c1c5729311e8ef601d330c79bbc5517f4fae1fa8
Mariya Moiseeva <mash.dobarina@gmail.com>
2023-07-17 16:11:31 +0300

    Создание компонента календарь

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -1,23 +1,46 @@
-import React from "react";
+import moment from "moment";
+import React, { useEffect, useState } from "react";
+import TrackingEvent from "./TrackingEvent";
+import { Button } from "react-bootstrap";
 
 export default function Calendar() {
-    const weekName = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'];
-    const monthNames = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-    const week = [1, 2, 3, 4, 5, 6, 7];
+    const totalDays = 42;
+    const weekName = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
+
+    const [startDay, setStartDay] = useState(moment().startOf('month').startOf('week'));
+    const [dates, setDates] = useState([]);
+    const array =[];
+    const day = startDay.clone();
+
+    useEffect(() => {
+        while(array.length < totalDays) {
+            array.push(day.clone());
+            day.add(1, 'day');
+        }
+        setDates(array);
+    },[startDay]);
+
     return (
         <>
             <div className="mt-3 ms-5" >
-                <div style={{display: 'grid', gridTemplateColumns: "repeat(7, auto)", gap: '10px'}}>
+                <div className="month-menu">
+                    <Button  variant="secondary">Пред. месяц</Button>
+                    <h2>Июль</h2>
+                    <Button variant="secondary">След. месяц</Button>
+                </div>
+                <div className="week-names">
                     {weekName.map(w => (
                         <h5>
                             {w}
                         </h5>
                     ))}
                 </div>
-                <div style={{display: 'grid', gridTemplateColumns: "repeat(7, auto)", gap: '10px'}}>
-                    {week.map(el => (
+                <div className="tracking-table">
+                    {dates.map(el => (
                         <div>
-                            {el}
+                            <TrackingEvent 
+                                date={el} 
+                                />
                         </div>
                     ) )}
                 </div>
