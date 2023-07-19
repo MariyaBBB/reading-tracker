ad3d9ee50e080cce5496ce247ca7623b1f3e3bd5
Mariya Moiseeva <mash.dobarina@gmail.com>
2023-07-19 14:33:32 +0300

    Настройка Router

--- src/App.js
+++ src/App.js
@@ -1,16 +1,22 @@
 import moment from "moment";
 import Body from "./components/Body";
 import Header from "./components/Header";
+import { BrowserRouter as Router, Route, Routes} from 'react-router-dom';
+import Calendar from "./components/Calendar";
+import Month from "./pages/Month";
 
 function App() {
   window.moment = moment;
   moment.updateLocale('en', {week: {dow: 1}});
   
   return (
-    <>
+    <Router>
       <Header />
-      <Body />
-    </>
+
+      <Routes>
+        <Route path="/month/:id" element={<Month />} />
+      </Routes>
+    </Router>
   );
 }
 
