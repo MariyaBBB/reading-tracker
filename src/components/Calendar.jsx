80e7b19124ded067eb05fc7030c9ddbc1813bcb4
Mariya Moiseeva <mash.dobarina@gmail.com>
2023-07-19 14:34:13 +0300

    Исправила Сalendar под Router

--- src/components/Calendar.jsx
+++ src/components/Calendar.jsx
@@ -2,31 +2,46 @@ import moment from "moment";
 import React, { useEffect, useState } from "react";
 import TrackingEvent from "./TrackingEvent";
 import { Button } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-export default function Calendar() {
+export default function Calendar({monthId}) {
     const totalDays = 42;
     const weekName = ['Пн', 'Вт', 'Ср', 'Чт', 'Пт', 'Сб', 'Вс'];
-
-    const [startDay, setStartDay] = useState(moment().startOf('month').startOf('week'));
+    const monthName = ['Январь', 'Февраль', 'Март', 'Апрель', 'Май', 'Июнь', 'Июль', 'Август', 'Сентябрь', 'Октябрь', 'Ноябрь', 'Декабрь'];
+    const month = moment().month(monthId).startOf('month').startOf('week') ;
     const [dates, setDates] = useState([]);
     const array =[];
-    const day = startDay.clone();
-
+    const day = month.clone();
+    const nextMonth = +monthId+1;
+    const prevMonth = +monthId-1;
+    console.log(nextMonth)
     useEffect(() => {
         while(array.length < totalDays) {
             array.push(day.clone());
             day.add(1, 'day');
         }
         setDates(array);
-    },[startDay]);
+    },[monthId]);
 
     return (
         <>
             <div className="mt-3 ms-5" >
                 <div className="month-menu">
-                    <Button  variant="secondary">Пред. месяц</Button>
-                    <h2>Июль</h2>
-                    <Button variant="secondary">След. месяц</Button>
+                    <Button 
+                        as={Link} 
+                        to={`/month/${prevMonth}`} 
+                        className={prevMonth < 0 ? "invisible": "visible pt-2 offset-0"} 
+                        variant="secondary">
+                            Пред. месяц
+                    </Button>
+                    <h2>{monthName[monthId]}</h2>
+                    <Button 
+                        as={Link} 
+                        to={`/month/${nextMonth}`} 
+                        className={nextMonth > 11 ? "invisible": "visible pt-2"} 
+                        variant="secondary">
+                            След. месяц
+                    </Button>
                 </div>
                 <div className="week-names">
                     {weekName.map(w => (
@@ -40,6 +55,7 @@ export default function Calendar() {
                         <div>
                             <TrackingEvent 
                                 date={el} 
+                                month={monthId}
                                 />
                         </div>
                     ) )}
