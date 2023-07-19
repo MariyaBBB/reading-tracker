b0ff3ac5cdf4a5120ff9b45b5b6a69d25776e655
Mariya Moiseeva <mash.dobarina@gmail.com>
2023-07-19 14:35:25 +0300

    Добавила меню для 2023 года, с выбором по месяцам

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,9 @@
 import React from "react";
-import { Container, Navbar, Nav } from "react-bootstrap";
+import { Container, Navbar, Nav, NavDropdown } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default function Header() {
+    const monthId = 5;
     return (
         <Navbar bg="dark" data-bs-theme="dark">
             <Container>
@@ -9,7 +11,20 @@ export default function Header() {
                     Reading Tracker
                 </Navbar.Brand>
                 <Nav className="me-auto">
-                    <Nav.Link href="#home">Январь</Nav.Link>
+                    <NavDropdown title="2023" id="basic-nav-dropdown">
+                        <NavDropdown.Item as={Link} to={`/month/${0}`}>Январь</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to={`/month/${1}`}>Февраль</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to={`/month/${2}`}>Март</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to={`/month/${3}`}>Апрель</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to={`/month/${4}`}>Май</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to={`/month/${5}`}>Июнь</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to={`/month/${6}`}>Июль</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to={`/month/${7}`}>Август</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to={`/month/${8}`}>Сентябрь</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to={`/month/${9}`}>Октябрь</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to={`/month/${10}`}>Ноябрь</NavDropdown.Item>
+                        <NavDropdown.Item as={Link} to={`/month/${11}`}>Декабрь</NavDropdown.Item>
+                    </NavDropdown>
                  </Nav>
             </Container>
         </Navbar>
